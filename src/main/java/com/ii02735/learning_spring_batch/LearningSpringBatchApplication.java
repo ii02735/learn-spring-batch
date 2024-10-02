03d98a60494fb3a076618895226ea0e24d37356e
Bilaal Yadallee <bilaal.yadallee@insee.fr>
2024-10-02 10:34:30 +0200

    Init Spring Batch application

---
+++ src/main/java/com/ii02735/learning_spring_batch/LearningSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.ii02735.learning_spring_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ii02735/learning_spring_batch/LearningSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ii02735.learning_spring_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
