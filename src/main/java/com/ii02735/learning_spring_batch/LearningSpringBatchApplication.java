002371c7f11ed46aa74c020b3d453fd6b2facfe1
ii02735 <yadallee.bilaal@outlook.fr>
2025-07-03 01:31:33 +0200

    Création d'un simple job et exécution au démarrage
    N.B :
    
    - L'annotation EnableBatchProcessing ne doit plus être utilisée depuis Spring Boot 3 (https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-3.0-Migration-Guide#enablebatchprocessing-is-now-discouraged), sinon elle empêche l'exécution des jobs au démarrage
    
    - On a enlevé spring.batch.job.enabled pour que le job puisse démarrer à l'exécution de l'application

--- src/main/java/com/ii02735/learning_spring_batch/LearningSpringBatchApplication.java
+++ src/main/java/com/ii02735/learning_spring_batch/LearningSpringBatchApplication.java
@@ -1,16 +1,36 @@
 package com.ii02735.learning_spring_batch;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @SpringBootApplication
-/*
- * Annotation nécessaire pour charger les beans nécessaires
- * au bon fonctionnement de Spring Batch
- */
-@EnableBatchProcessing
+@Slf4j
 public class LearningSpringBatchApplication {
+
+	@Bean
+	public Step firstStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+		return new StepBuilder("firstStep", jobRepository).tasklet(
+				(contribution, chunkContext) -> {
+					log.info("Executing firstStep : write any logic that you want");
+					return RepeatStatus.FINISHED;
+				}, transactionManager).build();
+	}
+
+	@Bean
+	public Job firstJob(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+		return new JobBuilder("firstJob", jobRepository)
+				.start(firstStep(jobRepository, transactionManager)).build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearningSpringBatchApplication.class, args);
