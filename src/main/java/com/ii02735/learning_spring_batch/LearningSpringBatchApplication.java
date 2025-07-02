26af33638b1f10a6b9d44e2b98da92d0806d8421
ii02735 <yadallee.bilaal@outlook.fr>
2025-07-03 00:46:46 +0200

    Ajout annotation @EnableBatchProcessing pour activer Spring Batch dans l'application

--- src/main/java/com/ii02735/learning_spring_batch/LearningSpringBatchApplication.java
+++ src/main/java/com/ii02735/learning_spring_batch/LearningSpringBatchApplication.java
@@ -1,9 +1,15 @@
 package com.ii02735.learning_spring_batch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/*
+ * Annotation nécessaire pour charger les beans nécessaires
+ * au bon fonctionnement de Spring Batch
+ */
+@EnableBatchProcessing
 public class LearningSpringBatchApplication {
 
 	public static void main(String[] args) {
